bc65f97ad40a0acf016e9f358ad3b1bc960bbd5b
Chloe Harris <chloe.a.harris17@gmail.com>
2021-10-16 14:51:18 -0700

    Updated index.js with required packages, classes, created empty arrays, outlined next steps for Inquirer prompt

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+// Require Employee classes 
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+
+// Require Generate HTML
+const GenerateHtml = require("./lib/generateHTML")
+
+// Create arrays to store user input 
+let managerArr = []
+let engineerArr = []
+let internArr = []
+
+
+// Inquirer Prompt Start
+
+// Manager Input, push User input to Manager Array 
+
+// Ask User how they want to proceed, provide Choices 
+
+// Engineer Input, push User input to Engineer Array 
+
+// Intern Input, push User input to Intern Array
