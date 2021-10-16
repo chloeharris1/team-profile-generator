fb0c2b7a47803a384290bc54a826c88cf13f60dc
Chloe Harris <chloe.a.harris17@gmail.com>
2021-10-16 15:37:03 -0700

    Created first prompt questions for manager info and next step user menu

--- index.js
+++ index.js
@@ -15,11 +15,66 @@ let engineerArr = []
 let internArr = []
 
 
-// Inquirer Prompt Start
+// Inquirer Prompt, Manager Info
+const start =() => {
+    inquirer.prompt([
+        {
+      type: "input",
+      message: "Enter the team manager’s name",
+      name: "managerName" 
+        },
+        {
+        type: "input",
+        message: "Enter their employee id",
+        name: "managerID" 
+        },
+        {
+        type: "input",
+        message: "Enter their email address",
+        name: "managerEmail" 
+        },
+        {
+        type: "input",
+        message: "Enter their office number",
+        name: "officeNumber" 
+        }
+    // Push User input to Manager Array 
+    ]).then(managerAns => {
+        const newManager = new Manager(managerAns.managerName, managerAns.managerID, managerAns.managerEmail, managerAns.officeNumber)
+        managerArr.push(newManager)
+        // Write to HTML
+        GenerateHtml.writeManager(managerArr)
+        // Proceed to next step
+        nextStep()
+    })
+}
 
-// Manager Input, push User input to Manager Array 
+// Start Inquirer Prompt 
+start()
 
-// Ask User how they want to proceed, provide Choices 
+// Provide User a menu with the option to add an engineer or an intern or to finish building their team using nextStep
+const nextStep = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do next?",
+            choices: ["Add an Engineer", "Add an Intern", "Finish"],
+            name: "nextStepChoice"
+        },
+        // If statement for User choice
+    ]).then(nextStepAns => {
+        if(nextStepAns.nextStepChoice === "Finish") {
+            GenerateHtml.writeFooter()
+            console.log("Goodbye")
+        }
+        if(nextStepAns.nextStepChoice === "Add an Engineer") {
+            addEngineer()
+        }
+        if(nextStepAns.nextStepChoice === "Add an Intern") {
+            addIntern()
+        }
+    })
+}
 
 // Engineer Input, push User input to Engineer Array 
 
